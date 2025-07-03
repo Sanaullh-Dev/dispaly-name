ca6167235fead65a1d76f4b3c5971257153ba1a0
Sanaulla Shaikh <sanaulla@0101digitall.com>
2025-07-03 23:50:17 +0530

    Implement full name display form with required fields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  // You must make the input fields such that the user is unable to submit without filling in both the fields.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" style={{ textAlign: "start", padding: "20px" }}>
+      <h2>Full Name Display</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const firstName = e.target.firstName.value;
+          const lastName = e.target.lastName.value;
+          document.getElementById(
+            "fullName"
+          ).innerText = `Full name: ${firstName} ${lastName}`;
+        }}
+      >
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            style={{ marginLeft: "10px", padding: "5px", width: "200px" }}
+            required
+            autoFocus
+          />
+        </label>
+        <br />
+        <br />
+        <label>
+          Last Name:&nbsp;
+          <input
+            type="text"
+            name="lastName"
+            style={{ marginLeft: "10px", padding: "5px", width: "200px" }}
+            required
+          />
+        </label>
+        <br />
+        <br />
+        <button
+          type="submit"
+          style={{ padding: "5px 20px", marginTop: "10px" }}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </form>
+      <div className="output">
+        <p id="fullName">Full name: John Doe</p>
+      </div>
     </div>
   );
 }
